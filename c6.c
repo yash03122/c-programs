7076fa9a55d57e1483c4fffde188158811561293
yash <yash03122@gmail.com>
2021-12-18 12:02:47 +0530

    addition of two number

---
+++ c6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    printf("entr a");
+    scanf("%d",&a);
+    printf("entr b");
+    scanf("%d",&b);
+    c=a+b;
+    printf("answer is %d",c);
+    getch();
+
+}
