d6db5f7c741ee03a022f8e218ba98916883fb46f
yash <yash03122@gmail.com>
2021-12-18 12:33:09 +0530

    area of circle

---
+++ c7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int r;
+    float area;
+    printf("enter the value of r");
+    scanf("%d",&r);
+    area=3.14*r*r;
+    printf("%f",area);
+    getch();
+}
