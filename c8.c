5b0fec1f85413e7b18cbc5daeb47d4e43776b8e2
yash <yash03122@gmail.com>
2021-12-25 17:18:50 +0530

    positive negative number

---
+++ c8.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    printf("enter the value of a");
+    scanf("%d",&a);
+    if(a>0)
+    {
+    printf("a is positive number");
+    }
+    else
+    {
+    printf("a is negative number");
+    }
+    getch();
+}
